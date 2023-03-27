c1420aa229d80ceb7e0e55a6c32c59d361799704
Weiberle17 <Max-Weiberle@gmx.de>
2023-03-27 16:21:37 +0200

    initial commit

---
+++ semesterFour/aufgabe8.php
@@ -0,0 +1,27 @@
+<html>
+  <body>
+    <?php
+      class Fahrzeug {
+        private $farbe;
+        private $hersteller;
+
+        public function __construct($farbe, $hersteller) {
+          $$this->farbe = $farbe;
+          $$this->hersteller = $hersteller;
+        }
+
+        public function Starten() {
+          echo "Das Fahrzeug ist gestartet <br>"
+        }
+
+        public function Stoppen() {
+          echo "Das Fahrzeug ist gestoppt <br>"
+        }
+      }
+
+      $t = new Fahrzeug("schwarz", "Toyota");
+      $t -> Starten();
+      $t -> Stoppen();
+    ?>
+  </body>
+</html>
